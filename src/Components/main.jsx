93b63e5c0c96639942ec8326832a3a636bc637b4
Saqalin Kazmi <saqlainkazmi12120@gmail.com>
2023-11-08 13:34:46 -0800

    work section done

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -16,7 +16,7 @@ import {FaTwitter, FaFacebook,FaInstagram, FaLinkedinIn} from 'react-icons/fa'
       <div className='w-full h-screen absolute top-0 left-0 bg-white/60'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
           <h1 className='sm:text-5xl text-4xl font-bold text-gray-800 '>This is React Web Page</h1>
-          <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+          <h2 className='flex sm:text-3xl text-2xl ml-10 pt-4 text-gray-800'>
             <TypeAnimation
       sequence={[
         // Same substring at the start will only be typed out once, initially
